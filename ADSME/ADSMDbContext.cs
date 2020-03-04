a530e95954511318f1234a9a0d93bc0c9544d067
Jiga9923 <jiga.9923@gmail.com>
2020-03-04 20:47:19 +0000

    Architecture & Design

---
+++ ADSME/ADSMDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using ADSM.Models;
+
+
+namespace ADSM
+{
+    public class ADSMDbContext : DbContext
+    {
+        public ADSMDbContext()
+            : base("name = ADSMDbContext") 
+
+        {
+            var objectcontext = (this as IObjectContextAdapter).ObjectContext;
+
+            objectcontext.CommandTimeout = 120;
+        }
+
+
+        /// <param name="modelBuilder"></param>
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // Stop lazy loading
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.AutoDetectChangesEnabled = false;
+            this.Configuration.ProxyCreationEnabled = true;
+            // App schema
+            modelBuilder.Entity<Models.User_Details>().ToTable("User_Details");
+
+        }
+
+        public DbSet<User_Details> Users { get; set; }
+    }
+}
